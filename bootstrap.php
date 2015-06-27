5b8c5eb0ec53676530f414c86de018a488436f21
Lewis Cowles <lewiscowles@me.com>
2015-06-27 20:34:08 +0100

    PHP Unit tests

---
+++ bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+function add_action( $name, callable $func ) {
+
+}
+
+function register_setting( $group, $name ) {
+
+}
+
+function plugins_url( $filepath, $relative_to='' ) {
+
+}
+
+function add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position ) {
+
+}
+
+function add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function ) {
+
+}
---
+++ test/main.php
@@ -0,0 +1,23 @@
+<?php
+
+use lewiscowles\WordPress\Utils\MenuPageHelper;
+
+class MainTest extends \PHPUnit_Framework_TestCase
+{
+	protected $menuHelper;
+
+	public function setUp()
+	{
+		$this->menuHelper = new MenuPageHelper();
+
+	}
+
+	public function testConstruct() {
+		$menuHelperInst = new MenuPageHelper();
+		$this->assertTrue( $menuHelperInst != $this->menuHelper );
+		$this->assertTrue( $menuHelperInst !== $this->menuHelper );
+		$this->assertTrue( $menuHelperInst instanceof  MenuPageHelper );
+		$this->assertTrue( $this->menuHelper instanceof  MenuPageHelper );
+	}
+
+}
