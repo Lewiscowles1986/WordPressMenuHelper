eb27c5a941710df643465f2d09d4c2e40acfcb50
Lewis Cowles <lewiscowles@me.com>
2015-06-28 19:35:37 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,21 +2,21 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 function add_action( $name, callable $func ) {
-  trigger_error('WordPress add_action', E_USER_NOTICE);
+  echo 'WordPress add_action';
 }
 
 function register_setting( $group, $name ) {
-  trigger_error('WordPress register_setting', E_USER_NOTICE);
+  echo 'WordPress register_setting';
 }
 
 function plugins_url( $filepath, $relative_to='' ) {
-  trigger_error('WordPress plugins_url', E_USER_NOTICE);
+  echo 'WordPress plugins_url';
 }
 
 function add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position ) {
-  trigger_error('WordPress add_menu_page', E_USER_NOTICE);
+  echo 'WordPress add_menu_page';
 }
 
 function add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function ) {
-  trigger_error('WordPress add_submenu_page', E_USER_NOTICE);
+  echo 'WordPress add_submenu_page';
 }
