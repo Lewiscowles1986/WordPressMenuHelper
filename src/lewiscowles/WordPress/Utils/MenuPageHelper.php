22ef1561431c901f75661b06f958f1aa549bfec1
Lewis Cowles <lewiscowles@me.com>
2015-06-28 17:25:24 +0100

    Re-Factored to make easier to test

--- src/lewiscowles/WordPress/Utils/MenuPageHelper.php
+++ src/lewiscowles/WordPress/Utils/MenuPageHelper.php
@@ -15,6 +15,14 @@ class MenuPageHelper {
 		\add_action( 'admin_init', array( $this, 'register_settings' ) );
 		\add_action( 'admin_menu', array( $this, 'add_menu' ), 99 );
 	}
+	
+	public function get_page_name() {
+		return isset($_GET['page']) ? strtolower( $_GET['page'] ) : 'default';
+	}
+	
+	public function get_page_data( $page ) {
+		return isset($this->_data[ $page ]) ? $this->_data[ $page ] : [];
+	}
 
 	public function add_menu() {
 		if( !is_array( $this->_data ) ) { throw new \Exception('There seems to be an issue with the format of our data'); return; }
@@ -34,73 +42,132 @@ class MenuPageHelper {
 			}
 		}
 	}
-
-	public function settings_page() {
-		$page = isset($_GET['page']) ? strtolower( $_GET['page'] ) : 'default';
-		if( !is_array( $this->_data ) ) { throw new \Exception('There seems to be an issue with the format of our data'); return; }
-		if(isset($_GET['settings-updated'])) {
-			if($_GET['settings-updated']) : ?>
+	
+	public function updated_view( array $data ) {
+		if(isset($data['settings-updated'])) {
+			if($data['settings-updated']) : ?>
 				<div id="message" class="updated fade"><p><strong>Settings Saved!</strong></p></div>
     		<?php else: ?>
         		<div id="message" class="error"><p><strong>Failure Saving Changes!</strong></p></div>
     		<?php endif;
     		}
-		$myOptions = isset($this->_data[ $page ]) ? $this->_data[ $page ] : [];
-		if( file_exists( $this->_dir.'/views/'.$page.'-page.php')) {
-			include_once $this->_dir.'/views/'.$page.'-page.php';
-		} else {
-			include_once $this->_dir.'/views/settings-page.php';
-		}
-	}
-	
-	public function getDataFilePath() {
-		return $this->_dir . '/data/menu.json';
 	}
 
-	public function menuIconURL( $string='' ) {
-		if( ( stripos($string, '/') !== false ) ) {
+	public function settings_page() {
+		$page = $this->get_page_name();
+		$this->updated_view( $_GET );
+		$this->render_page($page,$this->get_page_data( $page ));
+	}
+	
+	public function render_page($page,$myOptions) {
+		$specific_view = $this->_dir.'/views/'.$page.'-page.php';
+		if( file_exists( $specific_view ) ) {
+			include_once $specific_view;
+		} else { // switched to library built-in view (using.inc to avoid coverage for view code)
+			$built_in = __DIR__.'/views/settings-page.inc';
+			if( file_exists($built_in) ) {
+				include_once $built_in;
+			} else {
+				throw new \Exception('Built in View Removed, Don\'t modify Libraries... Extend them!');
+			}
+		}
+	}
+
+	public function parse_asset_url( $string='' ) {
+		$protocol_terminator_pos = stripos($string, '/');
+		if( ( $protocol_terminator_pos === false ) && ( stripos($string, 'dashicon') !== false ) ) { // not url or dashicon
 			return \plugins_url( $string, $this->_path );
-		} else {
+		} else { // dashicon or URL
 			return $string; // URL
 		}
 		return null;
 	}
-
-	public function build_menu( $optGroup ) {
-		if( !is_array( $this->_data ) ) { throw new Exception('There seems to be an issue with the format of our data'); return; }
-		$priority = ( isset( $optGroup['priority'] ) ? max( intval( $optGroup['priority'] ), 0) : 99 );
-
-		$menu_title = ( isset( $optGroup['menu_title'] ) ? $optGroup['menu_title'] : $optGroup['page_title'] );
-		$menu_img = ( isset( $optGroup['menuicon'] ) ? $this->menuIconURL( $optGroup['menuicon'] ) : null );
-
-		if( !isset( $optGroup['menu_submenu'] ) ) {
-			\add_menu_page(
-				$optGroup['page_title'],
-				$menu_title,
-				$optGroup['role'],
-				strtolower( $optGroup['name'] ),
-				array( $this, 'settings_page' ),
-				$menu_img,
-				$priority
-			);
-		} else {
-			\add_submenu_page(
-				$optGroup['menu_submenu'],
-				$optGroup['page_title'],
-				$menu_title,
-				$optGroup['role'],
-				strtolower( $optGroup['name'] ),
-				array( $this, 'settings_page' )
-			);
-		}
+	
+	public function parse_menu_icon( &$data ) {
+		return ( isset( $data['menuicon'] ) ? $this->parse_asset_url( $data['menuicon'] ) : null );
+	}
+	
+	public function parse_menu_title( &$data ) {
+		return ( isset( $data['menu_title'] ) ? $data['menu_title'] : $data['page_title'] );
+	}
+	
+	public function parse_menu_priority( &$data ) {
+		return ( isset( $data['priority'] ) ? max( intval( $data['priority'] ), 0) : 99 );
+	}
+	
+	public function parse_menu_data( &$data ) {
+		$out = [
+			'title' => $this->parse_menu_title( $data ),
+			'page_title' => $data['page_title'],
+			'sub_menu' => $data['menu_submenu'],
+			'role' => $data['role'],
+			'group' => strtolower( $data['name'] ),
+			'priority' => $this->parse_menu_priority( $data ),
+			'icon' => $this->parse_menu_icon( $data )
+		];
+		return $out;
+	}
+	
+	public function add_top_level_menu( &$data ) {
+		\add_menu_page(
+			$data['page_title'],
+			$data['title'],
+			$data['role'],
+			$data['group'],
+			array( $this, 'settings_page' ),
+			$data['icon'],
+			$data['priority']
+		);
+	}
+	
+	public function add_sub_menu( &$data ) {
+		\add_submenu_page(
+			$data['sub_menu'],
+			$data['page_title'],
+			$data['title'],
+			$data['role'],
+			$data['group'],
+			array( $this, 'settings_page' )
+		);
 	}
 
-	protected function option_fields() {
-		$menuData = @file_get_contents( $this->getDataFilePath() );
-		if( strlen($menuData."") > 0 ) {
-			$this->_data = json_decode( $menuData, true );
+	public function build_menu( $optGroup ) {
+		$data = $this->parse_menu_data( $optGroup );
+		if( !isset( $optGroup['menu_submenu'] ) ) {
+			$this->add_top_level_menu( $data );
 		} else {
-			throw new \Exception( "An issue loading the menu file '".$this->getDataFilePath()."'" );
+			$this->add_sub_menu( $data );
+		}
+	}
+	
+	public function get_data() {
+		return $this->_data;
+	}
+	
+	public function parse_data( $data ) {
+		return json_decode( $data, true );
+	}
+	
+	public function data_file_path() {
+		return $this->_dir . '/data/menu.json';
+	}
+	
+	public function get_data_file() {
+		try {
+			return file_get_contents( $this->data_file_path() );
+		} catch( \Error $e ) {
+			throw new \Exception( "An issue loading the menu file '".$this->data_file_path()."'" );
+		}
+		return "";
+	}
+
+	public function option_fields() {
+		$menuData = $this->get_data_file();
+		if( strlen($menuData."") > 0 ) {
+			$this->_data = $this->parse_data( $menuData );
+		} else {
+			trigger_error('Data file was malformed, falling back to empty array...', \E_USER_NOTICE );
+			$this->_data = [];
 		}
 	}
 }
