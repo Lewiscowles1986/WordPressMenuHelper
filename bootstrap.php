fa1a0485e565ec7b0aff6afbbf30fb392dd313e8
Lewis Cowles <lewiscowles@me.com>
2015-06-28 21:45:58 +0100

    Refined and added more tests

--- bootstrap.php
+++ bootstrap.php
@@ -2,21 +2,21 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 function add_action( $name, callable $func ) {
-
+  echo "WordPress add_action '{$name}'\n";
 }
 
 function register_setting( $group, $name ) {
-
+  echo "WordPress register_setting '{$setting}'->'{$name}'\n";
 }
 
 function plugins_url( $filepath, $relative_to='' ) {
-
+  echo "WordPress plugins_url\n";
 }
 
 function add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position ) {
-
+  echo "WordPress add_menu_page\n";
 }
 
 function add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function ) {
-
+  echo "WordPress add_submenu_page\n";
 }
--- test/main.php
+++ test/main.php
@@ -5,13 +5,13 @@ use lewiscowles\WordPress\Utils\MenuPageHelper;
 class MainTest extends \PHPUnit_Framework_TestCase
 {
 	protected $menuHelper;
+	
 
 	public function setUp()
 	{
 		$dir = __DIR__;
 		$path = __FILE__;
 		$this->menuHelper = new MenuPageHelper( $dir, $path );
-
 	}
 
 	public function testConstruct() {
@@ -44,11 +44,11 @@ class MainTest extends \PHPUnit_Framework_TestCase
 		$menuHelperInst->settings_page();
 	}
 	
-	public function testDataFilesExist() {
+	public function testDataPathExist() {
 		$this->assertTrue( file_exists( $this->menuHelper->data_file_path() ) );
 	}
 	
-	public function testDataFileFormat() {
+	public function testDataPathFormat() {
 		$this->assertContains( __DIR__, $this->menuHelper->data_file_path() );
 	}
 	
@@ -60,6 +60,119 @@ class MainTest extends \PHPUnit_Framework_TestCase
 		$this->assertFalse( $this->menuHelper->parse_data( '{bpb:[],}' ) === [] );
 		$this->assertFalse( $this->menuHelper->parse_data( null ) === [] );
 		$this->assertFalse( $this->menuHelper->parse_data( 12345 ) === [] );
+	}
+	
+	/**
+	 * @expectedException \Exception
+	 */
+	public function testGetDataFileNotExist() {
+		$menuHelperInst = new MenuPageHelper( ( '/notexist/'.time() ) , md5( time() ) );
+		$menuHelperInst->get_data_file();
+	}
+	
+	public function testGetDataFileExist() {
+		$this->menuHelper->get_data_file();
+	}
+	
+	/**
+	 * @expectedException \Exception
+	 */
+	public function testGetOptionFieldsInvalid() {
+		$menuHelperInst = new MenuPageHelper( ( '/notexist/'.time() ) , md5( time() ) );
+		$menuHelperInst->option_fields();
+	}
+	
+	public function testGetOptionFields() {
+		$this->menuHelper->option_fields();
+	}
+	
+	public function testGetPageData() {
+		$this->assertTrue( is_array( $this->menuHelper->get_page_data( 'nonexistant' ) ) );
+	}
+	
+	public function testGetPage() {
+		$this->assertEquals( $this->menuHelper->get_page_name(), 'default' );
+		$_GET['page'] = 'bob';
+		$this->assertEquals( $this->menuHelper->get_page_name(), 'bob' );
+	}
+	
+	/**
+	 * @expectedException \Exception
+	 */
+	public function testGetData() {
+		$menuHelperInst = new MenuPageHelper( ( '/notexist/'.time() ) , md5( time() ) );
+		$this->assertEquals( $menuHelperInst->get_data(), [] );
+	}
+	
+	public function testDashIconAsset() {
+		$this->assertEquals( $this->menuHelper->parse_asset_url('dashicons dashicons-format-quote'), 'dashicons dashicons-format-quote' );
+	}
+	
+	public function testURLIconAsset() {
+		$this->assertEquals( $this->menuHelper->parse_asset_url('//google.co.uk/bob.jpg'), '//google.co.uk/bob.jpg' );
+		$this->assertEquals( $this->menuHelper->parse_asset_url('http://google.co.uk/bob.jpg'), 'http://google.co.uk/bob.jpg' );
+		$this->assertEquals( $this->menuHelper->parse_asset_url('https://google.co.uk/bob.jpg'), 'https://google.co.uk/bob.jpg' );
+	}
+	
+	public function testLocalIconAsset() {
+		$this->assertTrue( $this->menuHelper->parse_asset_url('/google.co.uk/bob.jpg') !== '//google.co.uk/bob.jpg' );
+	}
+        
+        public function testMenuIconNull() {
+        	$payload = array('bob'=>'dylan');
+        	$this->assertTrue( is_null( $this->menuHelper->parse_menu_icon( $payload ) ) );
+        }
+        
+        public function testMenuIconExists() {
+        	
+        	$payload = array('menuicon'=>'dashicons dashicons-format-quote');
+        	$this->assertTrue( !is_null( $this->menuHelper->parse_menu_icon( $payload ) ) );
+        }
+
+	public function testParsingMenuTitle() {
+		$payload = array('menu_title'=>'munchkin');
+		$this->assertTrue( $this->menuHelper->parse_menu_title( $payload ) == 'munchkin' );
+		$payload = array('page_title'=>'bob');
+		$this->assertTrue( $this->menuHelper->parse_menu_title( $payload ) == 'bob' );
+		$payload = array('page_title'=>'bob','menu_title'=>'pappus');
+		$this->assertTrue( $this->menuHelper->parse_menu_title( $payload ) == 'pappus' );
+        }
+
+	public function testParseMenuPriority() {
+		$payload = array('priority'=>50);
+		$this->assertTrue( $this->menuHelper->parse_menu_priority( $payload ) == 50 );
+		$payload = array('priority'=>'jim');
+		$this->assertTrue( $this->menuHelper->parse_menu_priority( $payload ) == 0 );
+		$payload = array('foglight'=>50);
+		$this->assertTrue( $this->menuHelper->parse_menu_priority( $payload ) == 99 );
+        }
+
+	public function testMenuPage() {
+		$payload = array(
+			"title" => "Test", 
+			"page_title" => "settings",  
+			"sub_menu" => "settings", 
+			"role" => "administrator", 
+			"group" => "uniq-string", 
+			"priority" => 5, 
+			"icon" => "dashicons dashicons-format-quote" 
+		);
+		$this->expectOutputRegex( "/WordPress add_menu_page/" );
+		$this->menuHelper->add_top_level_menu( $payload );
+	}
+
+	public function testSubMenuPage() {
+		$payload = array(
+			"title" => "Test", 
+			"page_title" => "settings",  
+			"sub_menu" => "settings", 
+			"role" => "administrator", 
+			"group" => "uniq-string", 
+			"priority" => 5, 
+			"icon" => "dashicons dashicons-format-quote" 
+		);
+		$this->expectOutputRegex( "/WordPress add_submenu_page/" );
+		$this->menuHelper->add_sub_menu( $payload );
 	}
 
 }
